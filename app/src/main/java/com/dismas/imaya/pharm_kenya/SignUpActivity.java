79d35f23de2c9d203e949c19f8409cf2e4ebb407
ImayaDismas <imayadismas@gmail.com>
2016-06-23 23:57:28 +0300

    Sign up activity complete

--- app/src/main/java/com/dismas/imaya/pharm_kenya/SignUpActivity.java
+++ app/src/main/java/com/dismas/imaya/pharm_kenya/SignUpActivity.java
@@ -1,7 +1,42 @@
 package com.dismas.imaya.pharm_kenya;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by imaya on 6/23/16.
  */
-public class SignUpActivity {
+public class SignUpActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
 }
