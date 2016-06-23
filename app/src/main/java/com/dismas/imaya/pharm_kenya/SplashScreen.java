16bc148eb213bafd64b3c04190495dc82107e1d9
ImayaDismas <imayadismas@gmail.com>
2016-06-23 23:33:09 +0300

    The splash screen complete

---
+++ app/src/main/java/com/dismas/imaya/pharm_kenya/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.dismas.imaya.pharm_kenya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by imaya on 6/23/16.
+ */
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
